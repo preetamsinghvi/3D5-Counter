102af9e284296c4d2a022f5f141a9242a2a1b8a7
oscarcrowley1 <47679657+oscarcrowley1@users.noreply.github.com>
2020-02-07 15:54:33 +0000

    + project class

---
+++ KnittingApp/app/src/main/java/com/example/knittingapp/project.java
@@ -0,0 +1,11 @@
+public class Project {
+    string name;
+    int counter;
+    string description;
+
+    Project(string pName, string pDescription){
+        name = pName;
+        description = pDescription;
+        counter = 0;
+    }
+}
