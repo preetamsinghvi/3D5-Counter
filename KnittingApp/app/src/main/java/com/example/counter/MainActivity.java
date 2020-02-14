c47d0d70c4c453421d962bb76db20935aa05807f
Preetam Singhvi <preetamsinghvi@gmail.com>
2020-02-14 06:02:47 +0000

    Final Application

--- KnittingApp/app/src/main/java/com/example/counter/MainActivity.java
+++ KnittingApp/app/src/main/java/com/example/counter/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.knittingapp;
+package com.example.counter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -6,10 +6,21 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.icu.text.RelativeDateTimeFormatter;
 import android.os.Bundle;
 import android.text.InputType;
-import android.text.Layout;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.style.AlignmentSpan;
+import android.text.style.ForegroundColorSpan;
+import android.text.style.QuoteSpan;
+import android.text.style.RelativeSizeSpan;
+import android.text.style.StyleSpan;
+import android.text.style.UnderlineSpan;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -21,74 +32,82 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button addbutton;
     ArrayList<Project> projects;
     int rowCounter = 0;
     String projectN;
     String projectD;
-    SharedPreferences knitprefs;
+    SharedPreferences counterpref;
     TextView counter;
-    TextView projectName;
-    TextView description;
+    TextView counterName;
+    TextView counterDesc;
     boolean list = true;
     int currentId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_list);
-        knitprefs = getSharedPreferences("knitprefs",
-                MODE_PRIVATE);
+        setContentView(R.layout.activity_main);
 
+        addbutton = findViewById(R.id.addcounterbutton);
+
+        addbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), AddCounter.class);
+                startActivity(i);
+
+            }
+        });
+
+        counterpref = getSharedPreferences("counterpref",
+                MODE_PRIVATE);
 
         File file = new File(MainActivity.this.getFilesDir(), "projects");
         Log.e("myTag", file.getAbsolutePath());
-            if (!file.exists()) {
-                try {
-                    Log.e("File Fuckery", "File does not exist");
-                    file.createNewFile();
-                } catch (Exception e) {Log.e("File Fuckery", "File does not exist");
-                }
-            } else {Log.e("File Fuckery", "File exists");}
-
-            projects = new ArrayList<Project>();
+        if (!file.exists()) {
             try {
-                FileReader read = new FileReader(file);
-                BufferedReader inBuffer = new BufferedReader(read);
-                String line = inBuffer.readLine();
-                while(line != null){
-                    Log.e("File Fuckery", line);
-                    String[] filter = line.split("(;)");
-                    Log.e("File Fuckery", filter[1]);
-                    if(filter.length == 3){
-                        projects.add(new Project(filter[0], filter[1], Integer.parseInt(filter[2])));
-                    }
-                    line = inBuffer.readLine();
+                Log.e("File Error", "File does not exist");
+                file.createNewFile();
+            } catch (Exception e) {Log.e("File Error", "File does not exist");
+            }
+        } else {Log.e("File Error", "File exists");}
+
+        projects = new ArrayList<Project>();
+        try {
+            FileReader read = new FileReader(file);
+            BufferedReader inBuffer = new BufferedReader(read);
+            String line = inBuffer.readLine();
+            while(line != null){
+                Log.e("File Log", line);
+                String[] filter = line.split("(;)");
+                Log.e("File Log", filter[1]);
+                if(filter.length == 3){
+                    projects.add(new Project(filter[0], filter[1], Integer.parseInt(filter[2])));
                 }
-            } catch (Exception e) { Log.e("File Fuckery", e.getMessage());}
+                line = inBuffer.readLine();
+            }
+        } catch (Exception e) { Log.e("File Log", e.getMessage());}
 
-            if(projects.size()!=0){
-               projectN=projects.get(0).getName();
+        if(projects.size()!=0){
+            projectN=projects.get(0).getName();
 
-            } else projectN = "NAME";
+        } else projectN = "NAME";
 
-        Log.e("File Fuckery", Integer.toString(projects.size()));
-         if(list) {
-             refreshProjectList();
-         }
-         else {
-             setContentView(R.layout.activity_main);
-             refreshProjectPage();
-
-         }
+        Log.e("File Log", Integer.toString(projects.size()));
+        if(list) {
+            refreshProjectList();
         }
+        else {
+            setContentView(R.layout.counter_details);
+            refreshProjectPage();
 
-
+        }
+    }
     public void enterProjectName() {
         projectN="";
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
@@ -100,7 +119,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(DialogInterface dialog, int which) {
                 projectN = input.getText().toString();
-                projectName.setText(projectN);
+                counterName.setText(projectN);
                 projects.get(currentId).setName(projectN);
                 saveChanges();
             }
@@ -124,7 +143,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(DialogInterface dialog, int which) {
                 projectD = input.getText().toString();
-                description.setText(projectD);
+                counterDesc.setText(projectD);
                 projects.get(currentId).setDescription(projectD);
                 saveChanges();
             }
@@ -137,39 +156,46 @@ public class MainActivity extends AppCompatActivity {
         });
         builder.show();
     }
-
-
     void refreshProjectList(){
-        Button addNew = findViewById(R.id.button6);
+        Button addNew = findViewById(R.id.addcounterbutton);
         addNew.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent myIntent = new Intent(getBaseContext(), AddProject.class);
+                Intent myIntent = new Intent(getBaseContext(), AddCounter.class);
                 startActivity(myIntent);
             }
         });
         LinearLayout lay = findViewById(R.id.list);
         if(projects.size()==0){
             TextView tv=new TextView(getApplicationContext());
-            tv.setText("No projects yet :(");
+            tv.setText("No Counters Available");
             lay.addView(tv);
+            
         } else {
             for(int r=0; r<projects.size(); r++){
                 TextView tv=new TextView(getApplicationContext());
-                tv.setText(projects.get(r).getName());
+                String m = projects.get(r).getName();
+                String n = "--> ";
+                String s = n.concat(m);
+                int len = s.length();
+                SpannableString ss1=  new SpannableString(s);
+                ss1.setSpan(new RelativeSizeSpan(1.5f), 0,len, 0);
+                ss1.setSpan(new ForegroundColorSpan(Color.BLACK), 0, len, 0);
+                ss1.setSpan(new QuoteSpan(), 0, len, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+                tv.setText(ss1);
                 final int current = r;
                 tv.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
 
                         currentId = current;
-                        Log.e("File Fuckery", Integer.toString(currentId));
+                        Log.e("File Log", Integer.toString(currentId));
                         list = false;
                         rowCounter = projects.get(currentId).getCounter();
                         projectN =  projects.get(currentId).getName();
                         projectD = projects.get(currentId).getDescription();
 
-                        setContentView(R.layout.activity_main);
+                        setContentView(R.layout.counter_details);
                         refreshProjectPage();
 
                     }
@@ -179,14 +205,13 @@ public class MainActivity extends AppCompatActivity {
         }
 
     }
-
     void refreshProjectPage(){
         rowCounter = projects.get(currentId).getCounter();
         projectN =  projects.get(currentId).getName();
         projectD = projects.get(currentId).getDescription();
         counter = findViewById(R.id.textView);
-        projectName = findViewById(R.id.textView3);
-        description = findViewById(R.id.descr);
+        counterName = findViewById(R.id.textView3);
+        counterDesc = findViewById(R.id.descr);
 
         Button editName = findViewById(R.id.button3);
         editName.setOnClickListener(new View.OnClickListener() {
@@ -207,7 +232,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 saveChanges();
                 list = true;
-                setContentView(R.layout.activity_main_list);
+                setContentView(R.layout.activity_main);
                 refreshProjectList();
             }
         });
@@ -216,8 +241,8 @@ public class MainActivity extends AppCompatActivity {
         Button minusB = findViewById(R.id.button2);
 
         counter.setText(Integer.toString(rowCounter));
-        projectName.setText(projectN);
-        description.setText(projectD);
+        counterName.setText(projectN);
+        counterDesc.setText(projectD);
 
         plusB.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -244,9 +269,8 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
-
     void saveChanges(){
-        Log.e("Save Fuckery", "Got called");
+        Log.e("Save File", "Got called");
         try {
             File file = new File(MainActivity.this.getFilesDir(), "projects");
             FileWriter out = new FileWriter(file, false);
@@ -264,4 +288,4 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-}
+    }
--- KnittingApp/app/src/test/java/com/example/counter/ExampleUnitTest.java
+++ KnittingApp/app/src/test/java/com/example/counter/ExampleUnitTest.java
@@ -1,4 +1,4 @@
-package com.example.knittingapp;
+package com.example.counter;
 
 import org.junit.Test;
 
